4dd779f3a2c6ee50e5d987d9f0b5b797d958eeb4
Unknown <moonlight.shriram@gmail.com>
2020-07-20 21:16:08 +0400

    Formatted the mail being sent

--- GUI/Scripts/PrescriptionAppointmentRemedy.py
+++ GUI/Scripts/PrescriptionAppointmentRemedy.py
@@ -6,11 +6,11 @@ password = input("type your password: ")
 sender_email = "hackathon1231@gmail.com"
 reciever_email = "anandkrishna2312@gmail.com"
 
-message_relax = """You can take it easy and relax at home, your symptoms aren't too dangerous right now. Make sure you moniter your symtoms."""
+message_relax = """You can take it easy and relax at home, \nyour symptoms aren't too dangerous right now. \nMake sure you moniter your symtoms."""
 
-message_prescription = """The doctor will mail you a prescription for your symptoms."""
+message_prescription = """The doctor will mail you a prescription\n for your symptoms."""
 
-message_appointment = """I have booked an appointment for you at 5pm tomorrow at the nearest hospital."""
+message_appointment = """I have booked an appointment for you at 5pm tomorrow\n at the nearest hospital."""
 
 mail_prescription = """\
 Subject: Prescription for patient
@@ -18,7 +18,7 @@ Subject: Prescription for patient
 
 Please pass a prescription to the patient.
 Mail id: rishikumarikuf@gmail.com
-Patient details and symptoms:\n"""
+Patient details and symptoms:"""
 
 mail_appointment = """\
 Subject: Appointment for patient at 5pm
@@ -26,7 +26,7 @@ Subject: Appointment for patient at 5pm
 
 The patient has an appointment with you at 5pm
 Mail id: rishikumarikuf@gmail.com
-Patient details and symptoms:\n"""
+Patient details and symptoms:"""
 
 #Create a secure ssl context
 context = ssl.create_default_context()
@@ -38,17 +38,26 @@ def prepare_and_send_result_mail(severity, patient_data):  #Call this.
 
     elif 3<severity<7:
         mail = mail_prescription
-        for i in patient_data:
-            mail += i +'\n'
+        mail = prepare_mail(mail, patient_data)
         send_mail(mail)
         return message_prescription
-        
+
     elif severity>7:
         mail = mail_appointment
-        for i in patient_data:
-            mail += i +'\n'
+        mail = prepare_mail(mail, patient_data)
         send_mail(mail)
         return message_appointment
+
+def prepare_mail(mail, patient_data):
+    mail += "\nGender: " + patient_data[0]
+    mail += "\nAge: " + str(patient_data[1])
+    mail += "\nSymptoms: " + ','.join(patient_data[2])
+    if not patient_data[2]:
+        mail += "None"
+    mail += "\nPre-existing conditions: " + ','.join(patient_data[3])
+    if not patient_data[3]:
+        mail += "None"
+    return mail
 
 def send_mail(mail):  #Don't call this.
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
