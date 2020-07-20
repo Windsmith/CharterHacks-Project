79f80b4a1de34ba2ed1d6144f170de6ec18145d6
Rishabh69420 <65121903+Rishabh69420@users.noreply.github.com>
2020-07-20 13:04:32 +0400

    Added severity calculation
    Instructions are commented :)
    Example at the bottom

--- GUI/Scripts/Severity-Determiner.py
+++ GUI/Scripts/Severity-Determiner.py
@@ -1,31 +1,68 @@
+'''
+=================================================================================================================================================
+
+                                                  Tutorial on the usage of the class
+
+=================================================================================================================================================
+
+1) The methods under the Getter Methods are for getting either the question and option sets or the severity.
+   The get_set_<number>() methods returns a set of the questions in the form of a dictionary, key is the question and the values are the options
+   The get_severity() method returns the final severity of situation when called (Shri can make final verdict from it.)
+
+2) The methods under the Input Methods are for inputting the values (That Anand gets from user)
+   The result_<number>() method takes the input list as an arg, for each set of questions
+
+ # This shouldn't be interacted with #
+
+3) The method under the Weight Storage is for Weight Storage (obviously)
+
+PLEASE NOTE:= The order to use the methods should be input first, then get the severity
+              Also the options with number inputs will not have a value unless an input is made
+'''
+
 import math as M
 
 class Process:
+    
+    # Getter Methods
     def get_set_1(self):
-        multi = ""
         Pers_ques_and_optns = {
-            "What is your gender?" : [{multi : False}, ["Male", "Female", "Other", "Rather not say"]],
-            "What is your age?" : [{multi : False}, 10]
+            "What is your gender?" : [{"multi" : False}, ["Male", "Female", "Other", "Rather not say"]],
+            "What is your age?" : [{"multi" : False}, self.Pers_answers[1]]
         }
         return Pers_ques_and_optns
 
     def get_set_2(self):
-        multi = ""
         Symp_ques_and_optns = {
-            "What are your symptoms(if any)?" : [{multi : True}, ["Cough", "Fever", "Diarhhea", "Fatigue", "Headache", "Sore Throat", "Shortness of Breath", "Chest Pain", "Loss of Taste or Smell"]],
-            "Do you have any pre-existing conditions?" : [{multi : False}, ["Yes","No"]],
-            "What pre-existing condition do you have or have had?" : [{multi : True}, ["Any Heart condition", "All types of Cancer", "Obesity", "Cystic Fibrosis", "AIDS or any other Immunodeficiency", "Diabetes", "Asthma", "Hypertension", "Liver Disease"]],
+            "What are your symptoms(if any)?" : [{"multi" : True}, ["Cough", "Fever", "Diarhhea", "Fatigue", "Headache", "Sore Throat", "Shortness of Breath", "Chest Pain", "Loss of Taste or Smell"]],
+            "Do you have any pre-existing conditions?" : [{"multi" : False}, ["Yes","No"]],
+            "What pre-existing condition do you have or have had?" : [{"multi" : True}, ["Any Heart condition", "All types of Cancer", "Obesity", "Cystic Fibrosis", "AIDS or any other Immunodeficiency", "Diabetes", "Asthma", "Hypertension", "Liver Disease"]],
         }
         return Symp_ques_and_optns
 
     def get_set_3(self):
-        multi = ""
         Minor_ques_and_optns = {
-            "How many times have you left your house in the past 2 weeks?" : [{multi : False}, 2],
-            "Approximately how many separate people other than your family/room-mates have you come in close contact with?" : [{multi : False}, 20],
+            "How many times have you left your house in the past 2 weeks?" : [{"multi" : False}, self.Minor_answers[0]],
+            "Approximately how many separate people other than your family/room-mates have you come in close contact with?" : [{"multi" : False}, self.Minor_answers[1]],
         }
         return Minor_ques_and_optns
+    
+    def get_severity(self):
+        severity_counter = 0
+        for element in self.Pers_answers:
+            severity_counter += self.List_A_optn_weights[element]
+        
+        for element in self.Symp_answers:
+            if type(element) == list:
+                for option in element:
+                    severity_counter += self.List_B_optn_weights[option]
+        
+        for element in self.Minor_answers:
+            severity_counter += self.List_C_optn_weights[element]
 
+        return severity_counter
+
+    # Input Methods
     def result_1(self,answers:list):
         self.Pers_answers = answers
     
@@ -34,35 +71,34 @@ class Process:
 
     def result_3(self,answers:list):
         self.Minor_answers = answers
-    
+
+    # Weight storage (Not to be interacted with)
     def weights(self):
 
-        #initialization
+        # Initialization
         List_A = self.get_set_1()
         List_B = self.get_set_2()
         List_C = self.get_set_3()
 
-        #weight determination
+        # Weight determination
         List_A = list(List_A.values())
         List_B = list(List_B.values())
         List_C = list(List_C.values())
-        print(List_A[0][1][0])
-        print(List_A[1][1])
 
-        List_A_optn_weights = {
+        self.List_A_optn_weights = {
 
-            #weights of genders
+            # Weights of genders
             List_A[0][1][0] : 53/58,
             List_A[0][1][1] : 55/58,
             (List_A[0][1][2], List_A[0][1][3]) : 1,
             
-            #weight of age
+            # Weight of age
             int(List_A[1][1]) : M.exp(List_A[1][1] / 43.42944) / 10
         }
 
-        List_B_optn_weights = {                                          #allocating 3 points just to symptoms (subject to change)
+        self.List_B_optn_weights = {                                          # Allocating 3 points just to symptoms (subject to change)
             
-            #weights of symptoms
+            # Weights of symptoms
             List_B[0][1][0] : 0.8,
             List_B[0][1][1] : 0.68,
             List_B[0][1][2] : 0.2,
@@ -73,30 +109,28 @@ class Process:
             List_B[0][1][7] : 1.09,
             List_B[0][1][8] : 0.34,
 
-            #weights of pre-existing conditions
+            # Weights of pre-existing conditions
             List_B[2][1][0] : 0.97,
             List_B[2][1][1] : 1.02,
-            List_B[2][1][2] : 0.89,
+            List_B[2][1][2] : 0.93,
             List_B[2][1][3] : 1.1,
-            List_B[2][1][4] : 0.87,
-            List_B[2][1][5] : 0.86,
+            List_B[2][1][4] : 0.95,
+            List_B[2][1][5] : 0.9,
             List_B[2][1][6] : 0.62,
             List_B[2][1][7] : 0.57,
             List_B[2][1][8] : 0.59
         }
 
-        List_C_optn_weights = {
+        self.List_C_optn_weights = {
             List_C[0][1] : List_C[0][1] / 10,
             List_C[1][1] : List_C[1][1] / 25
         }
 
-        #tests
-        print(List_A, end = "\n\n")
-        print(List_B, end = "\n\n")
-        print(List_C, end = "\n\n")
-        print(List_A_optn_weights, end = "\n\n")
-        print(List_B_optn_weights, end = "\n\n")
-        print(List_C_optn_weights)
+# Example Test
 
 l = Process()
-l.weights()
+l.result_1(["Female", 50])
+l.result_2([["Cough", "Fever", "Fatigue", "Shortness of Breath"], "Yes", ["Cystic Fibrosis", "Hypertension"]])
+l.result_3([5,25])
+
+print(l.get_severity())
